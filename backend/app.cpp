835f1adaacc6fe097e049adc1d900c3d0e4421b4
PES2UG23CS538 <165575589+PES2UG23CS538@users.noreply.github.com>
2026-02-26 15:04:27 +0530

    Add HTTP server functionality to app.cpp
    Implement a basic HTTP server that responds with the hostname.

--- backend/app.cpp
+++ backend/app.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <unistd.h>
+#include <string>
+#include <netinet/in.h>
 
 int main() {
-    std::cout << "Hello from Lab-6 CI/CD Pipeline!" << std::endl;
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, 3);
+
+    while(true) {
+        int new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+
+        std::string response =
+            "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nServed by backend: ";
+
+        char hostname[1024];
+        gethostname(hostname, 1024);
+        response += hostname;
+
+        send(new_socket, response.c_str(), response.size(), 0);
+        close(new_socket);
+    }
+
     return 0;
 }
